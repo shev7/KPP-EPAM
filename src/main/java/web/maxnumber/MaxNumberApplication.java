18fde3e53acea81b1f3b5657829a02418acb1594
Egor Shinkarevich <yahorshinkarevich7@gmail.com>
2022-03-12 09:41:22 +0300

    initial commit

---
+++ src/main/java/web/maxnumber/MaxNumberApplication.java
@@ -0,0 +1,11 @@
+package web.maxnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxNumberApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MaxNumberApplication.class, args);
+    }
+}
---
+++ src/test/java/web/maxnumber/MaxNumberApplicationTests.java
@@ -0,0 +1,13 @@
+package web.maxnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxNumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
